39edf788faf4b3d11a2ffb89a974e2e470d02964
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-11-17 21:14:11 +0200

    testing facility

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Welcome to TripleX testing facility.";
+    std::cout << "In order to be granted access to the tools in this facility you must first pass a test. Get ready.";
     return 0;
 }
