43f81e2ba134787d7d79cf56f593438ffac49099
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-11-17 22:12:02 +0200

    add variables and use UE4 naming convention

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,7 +1,20 @@
 #include <iostream> 
 
 int main() {
-    std::cout << "Welcome to TripleX testing facility.";
-    std::cout << "In order to be granted access to the tools in this facility you must first pass a test. Get ready.";
+    // output introduction messages to player
+    std::cout << "Welcome to TripleX testing facility." << std::endl;
+    std::cout << "In order to be granted access to the tools in this facility you must first pass a test. Get ready." << std::endl;
+    std::cout << std::endl;
+
+    const int CodeA = 4;
+    const int CodeB = 2;
+    const int CodeC = 1;
+
+    const int sum = CodeA + CodeB + CodeC;
+    const int product = CodeA * CodeB * CodeC;
+
+    std::cout << "- The 3 secret code numbers add to: "<< sum << std::endl;
+    std::cout << "- They multiply together to be: " << product << std::endl;
+    
     return 0;
 }
